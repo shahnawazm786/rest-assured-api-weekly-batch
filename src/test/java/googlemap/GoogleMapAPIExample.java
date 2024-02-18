be504fde67ea2f325b1a6e4c6dcb6bfa8acc9315
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-02-18 22:28:18 +0530

    API learning

--- src/test/java/googlemap/GoogleMapAPIExample.java
+++ src/test/java/googlemap/GoogleMapAPIExample.java
@@ -43,23 +43,17 @@ public class GoogleMapAPIExample {
 
     }
     @Test
-    public void addLocationReturnResponseAsString(){
-        RestAssured.baseURI="https://rahulshettyacademy.com";
-        Response resp =given().header("Content-Type","application/json")
-                .queryParam("key","qaclick123")
+    public void addLocationReturnResponseAsString() {
+        RestAssured.baseURI = "https://rahulshettyacademy.com";
+        String resp = given().header("Content-Type", "application/json")
+                .queryParam("key", "qaclick123")
                 .log().all()
                 .body(Payload.addLocationPaylod())
                 .when()
                 .put("/maps/api/place/add/json")
                 .then()
-                .extract().response();
-        System.out.println(resp); // it will show object reference
-        System.out.println("=================== Response in string =============");
-        System.out.println(resp.toString());
-        System.out.println(String.valueOf(resp));
-        System.out.println(resp.getStatusCode());
-        System.out.println(resp.getBody());
-        System.out.println("=================== Response in string =============");
-        System.out.println(resp.asString());
-
+                .extract().response().asString();
+        System.out.println("====================== Response capture in String =========");
+        System.out.println(resp);
     }
+}
