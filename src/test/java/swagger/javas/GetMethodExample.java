95b97fd12ebdb8ced775266bbcef5fd30682cc25
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-02-18 21:12:44 +0530

    API learning

--- src/test/java/swagger/javas/GetMethodExample.java
+++ src/test/java/swagger/javas/GetMethodExample.java
@@ -6,8 +6,11 @@ public class GetMethodExample {
     //then() // response -
     public static void main(String[] args) {
     //postMethod();
-        postMethodWithResponse();
-        getMethod();
+      //  postMethodWithResponse();
+      //  getMethod();
+      // postMethodWithResponseAndRequest();
+       // updatePet();
+        deletePet();
     }
     static void getMethod(){
         given().header("Content-Type","application/json")
@@ -66,5 +69,62 @@ public class GetMethodExample {
                 .log().all()
                 .assertThat().statusCode(200);
 
+    }
+    static void postMethodWithResponseAndRequest(){
+        given().header("Content-Type","application/json")
+                .body("{\n" +
+                        "  \"id\": 0,\n" +
+                        "  \"category\": {\n" +
+                        "    \"id\": 0,\n" +
+                        "    \"name\": \"German Shepard - Demo\"\n" +
+                        "  },\n" +
+                        "  \"name\": \"doggie\",\n" +
+                        "  \"photoUrls\": [\n" +
+                        "    \"string\"\n" +
+                        "  ],\n" +
+                        "  \"tags\": [\n" +
+                        "    {\n" +
+                        "      \"id\": 0,\n" +
+                        "      \"name\": \"Demo\"\n" +
+                        "    }\n" +
+                        "  ],\n" +
+                        "  \"status\": \"available\"\n" +
+                        "}").log().all()
+                .when()
+                .post("https://petstore.swagger.io/v2/pet")
+                .then()
+                .log().all()
+                .assertThat().statusCode(200);
+
+    }
+
+    static void updatePet(){
+        given().header("Content-Type","application/json")
+                .body("{\n" +
+                        "  \"id\": 9223372036854251307,\n" +
+                        "  \"category\": {\n" +
+                        "    \"id\": 9223372036854251307,\n" +
+                        "    \"name\": \"German Shepard - update demo\"\n" +
+                        "  },\n" +
+                        "  \"name\": \"computer\",\n" +
+                        "  \"photoUrls\": [\n" +
+                        "    \"string\"\n" +
+                        "  ],\n" +
+                        "  \"tags\": [\n" +
+                        "    {\n" +
+                        "      \"id\": 9223372036854251307,\n" +
+                        "      \"name\": \"computer - demo\"\n" +
+                        "    }\n" +
+                        "  ],\n" +
+                        "  \"status\": \"available\"\n" +
+                        "}")
+                .when().put("https://petstore.swagger.io/v2/pet")
+                .then().log().all().assertThat().statusCode(200);
+    }
+    static void deletePet(){
+        //9223372036854251307
+        given().header("Content-Type","application/json")
+                .when().delete("https://petstore.swagger.io/v2/pet/9223372036854251307")
+                .then().log().all().assertThat().statusCode(200);
     }
 }
