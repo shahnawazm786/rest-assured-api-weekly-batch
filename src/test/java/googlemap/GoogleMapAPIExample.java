9d9999897f00533c97c451f6852aad7c94b5b865
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-02-25 00:01:08 +0530

    API learning

--- src/test/java/googlemap/GoogleMapAPIExample.java
+++ src/test/java/googlemap/GoogleMapAPIExample.java
@@ -4,6 +4,9 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
+
+import java.io.File;
+
 import static io.restassured.RestAssured.*;
 import static org.testng.Assert.assertEquals;
 
@@ -72,6 +75,27 @@ public class GoogleMapAPIExample {
         System.out.println(place_id);
         assertEquals("OK",status);
 
+    }
+    @Test
+    public void AddLocation1(){
+        RestAssured.baseURI = "https://rahulshettyacademy.com";
+        File file=new File("src/test/resources/location.json");
+        String resp = given().header("Content-Type", "application/json")
+                .queryParam("key", "qaclick123")
+                .log().all()
+                .body(file)
+                .when()
+                .put("/maps/api/place/add/json")
+                .then()
+                .extract().response().asString();
+        System.out.println("====================== Response capture in String =========");
+        System.out.println(resp);
+
+        JsonPath path=new JsonPath(resp);
+       // System.out.println(path.getString("Content-Type"));
+        System.out.println(path.getString("status"));
+        System.out.println(path.getString("scope"));
+        System.out.println(path.getString("place_id"));
     }
 
 
