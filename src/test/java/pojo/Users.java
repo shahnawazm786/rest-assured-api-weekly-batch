5ed9546c2f0ea49dfc2645c69e5a2cd2bf89c983
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-03-02 09:51:18 +0530

    API learning

---
+++ src/test/java/pojo/Users.java
@@ -0,0 +1,42 @@
+package pojo;
+
+public class Users {
+    private String name;
+    private String job;
+    private int id;
+    private String createdAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+}
