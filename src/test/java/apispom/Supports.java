3ead14e21e2699f6c6117c8197d494622a3038a2
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-03-02 10:33:14 +0530

    API learning

---
+++ src/test/java/apispom/Supports.java
@@ -0,0 +1,28 @@
+package apispom;
+
+public class Supports{
+    private String url;
+    private String text;
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+   /* public Supports(String url,String text){
+        this.url=url;
+        this.text=text;
+    }*/
+}
+
