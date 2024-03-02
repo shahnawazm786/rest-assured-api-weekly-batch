76379f59d7df9228d1ae767e7ff6180799be751c
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-03-02 10:33:21 +0530

    API learning

---
+++ src/test/java/steps/ReqResSteps.java
@@ -0,0 +1,24 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ReqResSteps {
+    @Then("validate value")
+    public void validateValue() {
+
+    }
+
+    @When("user provide {string}")
+    public void userProvideId() {
+
+    }
+
+    @Given("user hit the get api")
+    public void userHitTheGetApi() {
+
+    }
+
+
+}
